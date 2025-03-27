a4fafa7f6d930d990d30bb52aa43b7135c886f05
Squ1nty <vincentdao288@gmail.com>
2025-03-27 11:26:55 +1100

    'Initial commit' is testing the logic for reversing an array.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args){
+        reversingArray();
+    }
+
+    public static void reversingArray(){
+        /*
+            Key issue was a Logic Error, intially I was traversing the whole array,
+            however, logically you only need to traverse half of it; or 1 less than 
+            half if the length of the array is odd 
+        */
+
         int[] myList = {1, 2, 3, 4, 5};
 
         for(int i = 0, j = myList.length - 1; i < myList.length / 2; i++, j--){
